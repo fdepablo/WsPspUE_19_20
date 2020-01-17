1ab0a2e2c508b2a017b22f4e1ab9e349f6fb3c28
profesoresi <profesoresi@alumnos.uem.es>
2020-01-17 20:00:02 +0100

    add ejemplos spring core 17/01/2020

--- 06_Spring/src/main/java/main/MainSpring.java
+++ 06_Spring/src/main/java/main/MainSpring.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,13 +14,13 @@ public class MainSpring {
 	public static void main(String[] args) {
 		context = new ClassPathXmlApplicationContext("beans.xml");
 		
-		Persona p1 = new Persona();
-		p1.setNombre("pepe");
+		//Persona p1 = new Persona();
+		//p1.setNombre("pepe");
 		
 		Persona p2 = (Persona)context.getBean("persona1");
 		System.out.println(p2);
 		
-		p1 = null;
+		//p1 = null;
 		p2 = null;
 		
 		Persona p3 = context.getBean("persona1",Persona.class);
@@ -26,6 +28,25 @@ public class MainSpring {
 		p3.setPeso(70);
 		Persona p4 = context.getBean("persona1",Persona.class);
 		System.out.println(p4);
+		
+		Persona pBean1 = context.getBean("personaBean",Persona.class);
+		
+		List<Persona> listaPersonas = context.getBean("listaPersonas",List.class);
+		listaPersonas.add(pBean1);
+		
+		Persona esneip =  context.getBean("esneip",Persona.class);
+		System.out.println(esneip);
+		
+		Persona jagri =  context.getBean("jagri",Persona.class);
+		System.out.println(jagri);
+		
+		Persona sirius =  context.getBean("personaConstructor",Persona.class);
+		System.out.println(sirius);
+		
+		List<Persona> listaPersonas2 = context.getBean("listaPersonas2",List.class);
+		for(Persona p : listaPersonas2) {
+			System.out.println(p);
+		}
 	}
 
 }
--- 06_Spring/src/main/java/modelo/entidad/Persona.java
+++ 06_Spring/src/main/java/modelo/entidad/Persona.java
@@ -1,9 +1,24 @@
 package modelo.entidad;
 
+//Para poder ser gestionado por spring deben de tener un constructor
+//privado y sus atributos deben de ser propeidades, es decir, getters y 
+//setters
 public class Persona {
 	private String nombre;
 	private int edad;
 	private double peso;
+	private Direccion direccion;
+	
+	public Persona() {
+		super();
+	}
+	public Persona(String nombre, int edad, double peso, Direccion direccion) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+		this.peso = peso;
+		this.direccion = direccion;
+	}
 	
 	public String getNombre() {
 		return nombre;
@@ -24,9 +39,17 @@ public class Persona {
 		this.peso = peso;
 	}
 	
+	public Direccion getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(Direccion direccion) {
+		this.direccion = direccion;
+	}
 	@Override
 	public String toString() {
-		return "Persona [nombre=" + nombre + ", edad=" + edad + ", peso=" + peso + "]";
+		return "Persona [nombre=" + nombre + ", edad=" + edad + ", peso=" + peso + ", direccion=" + direccion + "]";
 	}
-		
+
+	
+	
 }
